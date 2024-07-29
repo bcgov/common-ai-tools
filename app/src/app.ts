9cd38b62f6d3b538b1dfc9e792f4717628633110
Csaky <Tim.Csaky@gov.bc.ca>
2024-07-29 15:55:46 -0700

    create and query endpoints

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,27 +1,27 @@
-// Import the 'express' module along with 'Request' and 'Response' types from express
 import express, { NextFunction, Request, Response } from 'express';
 
 import controller  from './controller';
 
 // Create an Express application
 const app = express();
-
-// Specify the port number for the server
 const port: number = 3000;
+// handle json in request body
+app.use(express.json());
 
-// Define a route for the root path ('/')
+// -------- routes
 app.get('/', (req: Request, res: Response) => {
   res.send('');
 });
 
-app.get('/create', (req: Request, res: Response, next: NextFunction): void => {
+app.put('/create', (req: Request, res: Response, next: NextFunction): void => {
   controller.create(req, res, next);
 });
 
+app.get('/query', (req: Request, res: Response, next: NextFunction): void => {
+  controller.query(req, res, next);
+});
 
-
-// Start the server and listen on the specified port
+// ---------- Start server and listen on the specified port
 app.listen(port, () => {
-  // Log a message when the server is successfully running
   console.log(`Server is running on http://localhost:${port}`);
 });
--- app/src/controller.ts
+++ app/src/controller.ts
@@ -1,16 +1,84 @@
-
+// @ts-nocheck
 import 'dotenv/config'
 import type { NextFunction, Request, Response } from 'express';
 
+// Langchain AI modules
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import {
+  RunnableSequence,
+  RunnablePassthrough,
+} from "@langchain/core/runnables";
+import { formatDocumentsAsString } from "langchain/util/document";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
 
 const controller = {
-  
-  create: async (_req: Request, res: Response, next: NextFunction) => {
-
-
-
+  /**
+   * Create Vector embeddings based on a document source (RAG)
+   * @param _req 
+   * @param res 
+   * @param next 
+   */
+  create: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const response: unknown = process.env.TEST;
+      // --------- get documents from GitHub repo source, using Github API
+      // ref: https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#get-repository-content
+      const { Octokit: Octokit } = await import('octokit');
+      // authenticate - use a personal token, authorized for bcgov org
+      const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN,
+      });
+      // get list of wiki pages
+      const repoFiles: unknown = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+        owner: req.body.owner,
+        repo: req.body.repo,
+        path: req.body.path,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      });
+      const urlArray = repoFiles.data
+        .filter((file) => file.type === 'file')
+        .map((file) => file.download_url);
+
+        let response: unknown = {
+          owner: req.body.owner,
+          repo: req.body.repo,
+          path: req.body.path,
+          urlCount: urlArray.length,
+          urls: []
+        };
+        
+      // add vectors to a database
+      for (const url of urlArray) {
+        // get document
+        const loader = new CheerioWebBaseLoader(url);
+        const document = await loader.load();
+
+        const splitter = RecursiveCharacterTextSplitter.fromLanguage("markdown", {
+          chunkSize: 500,
+          chunkOverlap: 50
+        });
+        const splits = await splitter.splitDocuments([document]);
+        console.log(`splits ${splits}`);
+        
+        // create vectors in chromadb database (running in separate container)
+        const vectorStore = await Chroma.fromDocuments(
+          splits,
+          new OpenAIEmbeddings(),
+          {
+            collectionName: req.body.repo,
+            url: "http://localhost:8000", // Optional, will default to this value
+          }
+        );
+        console.log(`vectorStore ${vectorStore}`);
+        response.urls.push(url)
+      }
+      
       res.status(200).send(response);
       
     } catch (e: unknown) {
@@ -18,9 +86,41 @@ const controller = {
     }
   },
 
-  query: async (_req: Request, res: Response, next: NextFunction) => {
+  query: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const response: unknown = 'query';
+
+      const model = new ChatOpenAI({});
+      const vectorStore = await Chroma.fromExistingCollection(
+        new OpenAIEmbeddings(),
+        { collectionName: req.body.repo }
+      );
+      const retriever = vectorStore.asRetriever();
+      const prompt =
+        PromptTemplate.fromTemplate(`Answer the question based only on the following context:
+      {context}
+  
+      Question: {question}`);
+  
+      const chain = RunnableSequence.from([
+        {
+          context: retriever.pipe(formatDocumentsAsString),
+          question: new RunnablePassthrough(),
+        },
+        prompt,
+        model,
+        new StringOutputParser(),
+      ]);
+  
+      console.log(req.body.question);
+
+      const result = {
+        owner: req.body.owner,
+        repo: req.body.repo,
+        path: req.body.path,
+        answer: await chain.invoke(req.body.question)
+      };
+    
+      const response: Object = result;
       res.status(200).send(response);
       
     } catch (e: unknown) {
