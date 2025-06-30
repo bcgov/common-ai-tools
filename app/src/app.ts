ed6b95a72537cd41f2f59f6575728b04af539609
Csaky <Tim.Csaky@gov.bc.ca>
2025-06-30 13:25:56 -0700

    test pdf index/query

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import express, { NextFunction, Request, Response } from 'express';
+const multer  = require('multer')
 
 import controller  from './controller';
 
@@ -8,18 +9,27 @@ const app = express();
 const port: number = 3000;
 // handle json in request body
 app.use(express.json());
+const upload = multer({ dest: '../uploads/' })
 
 // -------- routes
 app.get('/', (req: Request, res: Response) => {
   res.send('');
 });
 
-app.put('/create', (req: Request, res: Response, next: NextFunction): void => {
+app.put('/create/github', (req: Request, res: Response, next: NextFunction): void => {
   controller.create(req, res, next);
 });
 
-app.get('/query', (req: Request, res: Response, next: NextFunction): void => {
+app.get('/query/github', (req: Request, res: Response, next: NextFunction): void => {
   controller.query(req, res, next);
+});
+
+app.post('/index/pdf', upload.single('pdf'), (req: Request, res: Response, next: NextFunction): void => {
+  controller.upload(req, res, next);
+});
+
+app.get('/query/pdf', (req: Request, res: Response, next: NextFunction): void => {
+  controller.question(req, res, next);
 });
 
 // ---------- Start server and listen on the specified port
--- app/src/controller.ts
+++ app/src/controller.ts
@@ -1,11 +1,11 @@
 // @ts-nocheck
 import 'dotenv/config'
 import type { NextFunction, Request, Response } from 'express';
-
 // Langchain AI modules
 // import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 
 import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { Chroma } from "@langchain/community/vectorstores/chroma";
@@ -20,15 +20,80 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 
 
 const controller = {
+
   /**
-   * Create Vector embeddings based on a document source (RAG)
-   * @param _req 
-   * @param res 
-   * @param next 
+   * index content in pdf's
+   */
+  upload: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const loader = new PDFLoader(req.file.path);
+      const docs = await loader.load();
+      // add vectors to a database
+      let splits;
+      for (const doc of docs) {
+        const textSplitter = new RecursiveCharacterTextSplitter({
+          chunkSize: 500,
+          chunkOverlap: 50
+        });
+        splits = await textSplitter.createDocuments([doc.pageContent]);
+        console.dir(splits, { depth: null });
+        // create vectors in chromadb database (running in separate container)
+        const vectors = await Chroma.fromDocuments(
+          splits,
+          new OpenAIEmbeddings(),
+          {
+            // collectionName: req.body.repo,
+            collectionName: req.query.formId,
+            url: "http://localhost:8000", // Optional, will default to this value
+          }
+        );
+        console.dir(vectors, { depth: null });
+      }
+      res.sendStatus(201, splits.length);
+    } catch (e: unknown) {
+      next(e);
+    }
+  },
+
+  /**
+   * query content from pdf's
+   */
+  question: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const model = new ChatOpenAI({});
+      const vectorStore = await Chroma.fromExistingCollection(
+        new OpenAIEmbeddings(),
+        { collectionName: `${req.body.formId}` }
+      );
+      const retriever = vectorStore.asRetriever();
+      const prompt =
+        PromptTemplate.fromTemplate(`Answer the question based only on the following context:
+      {context}
+      Question: {question}`);
+      const chain = RunnableSequence.from([
+        {
+          context: retriever.pipe(formatDocumentsAsString),
+          question: new RunnablePassthrough(),
+        },
+        prompt,
+        model,
+        new StringOutputParser(),
+      ]);
+      const response = {
+        formId: `${req.body.formId}`,
+        answer: await chain.invoke(req.body.question)
+      };
+      res.status(200).send(response);
+    } catch (e: unknown) {
+      next(e);
+    }
+  },
+
+  /**
+   * index content in a github repo
    */
   create: async (req: Request, res: Response, next: NextFunction) => {
     try {
-
       const data = {
         repo: req.body.repo, // eg: "https://github.com/bcgov/common-object-management-service-docs"
         branch: req.body.branch ?? 'main', // eg: "main"
@@ -46,22 +111,17 @@ const controller = {
         unknown: "warn",
         maxConcurrency: 5, // Defaults to 2
       };
-
-      console.log('config', config);
-
       const loader = new GithubRepoLoader(
         data.repo, config
       );
       const docs = await loader.load();
-      
       // reformat
       const documents = docs.map(d => {
         return {
           file: d.metadata.source,
           pageContent: d.pageContent
         };
-     });
-
+      });
       // let response = '';
       let response: unknown = {
         repo: req.body.repo,
@@ -73,17 +133,14 @@ const controller = {
         vectorStore: `${req.body.repo}/${req.body.path}`,
         vectorCount: 0
       };
-        
       // add vectors to a database
       for (const doc of documents) {
-
-        const textSplitter = new RecursiveCharacterTextSplitter({ 
+        const textSplitter = new RecursiveCharacterTextSplitter({
           chunkSize: 500,
-          chunkOverlap: 50 
+          chunkOverlap: 50
         });
         const splits = await textSplitter.createDocuments([doc.pageContent]);
         console.log(`splits ${splits.length}`);
-      
         // create vectors in chromadb database (running in separate container)
         await Chroma.fromDocuments(
           splits,
@@ -96,22 +153,21 @@ const controller = {
         );
         response.vectorCount = response.vectorCount + splits.length;
       }
-      
       res.status(200).send(response);
-      
     } catch (e: unknown) {
       next(e);
     }
   },
 
+  /**
+   * query content in a github repo
+   */
   query: async (req: Request, res: Response, next: NextFunction) => {
     try {
-
       const model = new ChatOpenAI({});
       const vectorStore = await Chroma.fromExistingCollection(
         new OpenAIEmbeddings(),
-        // { collectionName: req.body.repo }
-        { collectionName: 'test1' }
+        { collectionName: `${/[^/]*$/.exec(req.body.repo)[0]}` }
       );
       const retriever = vectorStore.asRetriever();
       const prompt =
@@ -119,7 +175,7 @@ const controller = {
       {context}
   
       Question: {question}`);
-  
+
       const chain = RunnableSequence.from([
         {
           context: retriever.pipe(formatDocumentsAsString),
@@ -129,19 +185,14 @@ const controller = {
         model,
         new StringOutputParser(),
       ]);
-  
-      console.log(req.body.question);
-
       const result = {
         repo: req.body.repo,
         branch: req.body.branch,
         path: req.body.path,
         answer: await chain.invoke(req.body.question)
       };
-    
       const response: Object = result;
       res.status(200).send(response);
-      
     } catch (e: unknown) {
       next(e);
     }
